0fc4807b5892898dda6c119a295f684b19f3ef07
s6makano <s6makano@uni-bonn.de>
2016-07-12 18:28:01 +0200

    mklnl

--- Memtest.hs
+++ Memtest.hs
@@ -4,7 +4,7 @@ import Prelude
      
 fiblist = map fib [0 ..]
 
-fib 0 = (\a b -> a)
-fib 1 = (\a b -> b)
-fib n = (\a b -> (fiblist !! (n-2)) a b + (fiblist !! (n-1)) a b)
+fib 0 = 0
+fib 1 = 1
+fib n = fiblist !! (n-2) + fiblist !! (n-1)
          
