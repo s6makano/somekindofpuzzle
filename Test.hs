058b7a0825a0722e00830563350dbbed9a2dc711
x432ph <x432ph@gmx.de>
2016-07-13 03:16:56 +0200

    Memory-Zwischenbericht
    Also hier sind zwei Lösung, von denen ich recht überzeugt bin, dass es
    viel besser nicht geht, in der Test.hs. Ich denke, untere Umsetzung ist
    besser, da bei ihr nicht die Gefahr besteht, dass man vergisst, in der
    Rekursion die Funktion mit Gedächtnis zu benutzen.

--- Test.hs
+++ Test.hs
@@ -1,38 +1,34 @@
 module Test where
 import Prelude
 import System.Random
-
-
-     
-fiblist = map fib [0 ..]
-
-fib 0 = 0
-fib 1 = 1
-fib n = fiblist !! (n-2) + fiblist !! (n-1)
-         
-         
-dfib :: IO (Int -> Integer)
-dfib = do a <- randomRIO (1,1)
-          b <- randomRIO (1,1)
-          let fib :: Int -> Integer
-              fib n = case () of _
-                                                    | n==0 -> a
-                                                    | n==1 -> b
-                                                    | otherwise -> fiblist !! (n-1) + fiblist !! (n-2)
-              fiblist = map fib [0..]
-          print1 fib 900
-          print1 fib 1800
-          print1 fib 3600
+import Data.Function.Memoize
+       
+dfib :: IO (Integer -> Integer)
+dfib = do a <- randomRIO (1,100)
+          b <- randomRIO (1,100)
+{-          let fib' :: Integer -> Integer
+              fib' 0 = a
+              fib' 1 = b
+              fib' n = fib (n-1) + fib (n-2)
+              fib :: Integer -> Integer
+              fib = memoize fib'      <--      funktioniert. -}
+          let fib' :: (Integer -> Integer) -> Integer -> Integer
+              fib' fib 0 = a
+              fib' fib 1 = b
+              fib' fib n = fib (n - 1) + fib (n-2)
+              fib :: Integer -> Integer
+              fib = memoFix fib'   {-   <--      funktioniert auch. -}
+              
+              
+          print1 fib 100000
+          print1 fib 200000
           return fib
 
 
 print1  fib n = putStrLn $ show $ fib n
-print2 n fib = putStrLn $ show $ fib $ n+1
-print3 n fib = putStrLn $ show $ fib $ n+2
-    
-hilfe:: IO ()
-hilfe = do fib <-dfib
-           putStrLn $ show $ fib 900
-           putStrLn $ show $ fib $ 900*2
-           putStrLn $ show $ fib $ 900*2*2
-           
+
+test = do fib <- dfib
+          print1 fib 200000
+          
+
+          
