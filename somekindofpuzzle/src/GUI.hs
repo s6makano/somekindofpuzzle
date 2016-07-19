ebcad42eab8755a12332fb5daccc45c2e12bc259
s6makano <s6makano@uni-bonn.de>
2016-07-19 23:40:34 +0200

    Working version
    Compile Main.hs in src or run existing .exe

---
+++ somekindofpuzzle/src/GUI.hs
@@ -0,0 +1,119 @@
+module GUI where
+import Graphics.UI.WX hiding (when)
+import Graphics.UI.WXCore as WXCore hiding (when)
+import Prelude
+import Logic
+import Data.List
+import System.Random
+import Control.Monad
+
+
+picsize :: Int
+picsize = 100
+
+gui :: IO ()
+gui = 
+   do
+     vs <- varCreate 0   
+     level <- createLevel 0
+     vlevel <- varCreate level
+     
+     
+     f <- frame   [ resizeable := False ]
+      
+     status <- statusField [text := "DAS STEHT UNTEN"] 
+     set f [statusBar := [status]] 
+     
+     
+     game <- menuPane       [ text := "&Game" ] 
+     new  <- menuItem game  [ text := "&New\tCtrl+N" 
+                            , help := "New game"
+                            ]  
+
+     
+     menuLine game 
+     quit <- menuQuit game [help := "Quit the game"] 
+
+     set new   [on command := do 
+                                 newlevel <- createLevel 0
+                                 varSet vs 0
+                                 varSet vlevel newlevel 
+                                 set f [ layout      := space (fst (sizel newlevel)*picsize) (snd (sizel newlevel)*picsize)]
+                                 repaint f             
+               ]
+     set quit  [on command := close f] 
+
+     set f [menuBar := [game]] 
+       
+     set f [ text        := "somekindofpuzzle"
+           , bgcolor     := white 
+           , layout      := space (fst (sizel level)*picsize) (snd (sizel level)*picsize) {- Variabel? -}           
+           , on paint    := onpaint f vlevel 
+                        
+           , on click    := clicki f vlevel vs
+           ] 
+     WXCore.windowSetFocus f
+
+onpaint :: Frame () -> Var Level -> DC a -> b -> IO ()
+onpaint f vlevel dc _view = do set f [on paint := onpaint{-debug-} f vlevel]
+                               {-putStrLn "Was passiert hier?"-}
+                               draw vlevel dc _view
+
+onpaintdebug :: Frame () -> Var Level -> DC a -> b -> IO ()
+onpaintdebug f vlevel dc _view = set f [on paint := onpaint f vlevel]
+                               
+
+clicki :: Frame () -> Var Level -> Var Int -> Point -> IO()
+clicki f vlevel vs (Point b h) = do {-putStrLn "Ich wurde angeklickt."-}
+                                    level <- varGet vlevel
+                                    {-putStrLn "Ich beginne, einen neuen Level zu generieren."-}
+                                    varSet vlevel (input level (ceiling (fromIntegral b/fromIntegral picsize), ceiling (fromIntegral h/fromIntegral picsize)))
+                                    level2 <- varGet vlevel
+                                    {-putStrLn "Der neue Level ist da."
+                                    putStrLn "Jetzt teste ich noch, ob die Siegbedingung erfüllt ist."-}
+                                    when (finito level2) $ do s <- varGet vs
+                                                              varSet vs $ s+1
+                                                              newlevel <- createLevel $ s+1
+                                                              varSet vlevel newlevel
+                                                              set f [ layout := space (fst (sizel newlevel)*picsize) (snd (sizel newlevel)*picsize)]
+                                    {-putStrLn "Siegbedingung getestet."-}
+                                    repaint f
+                                 
+                                 
+    
+draw :: Var Level -> DC a -> b -> IO ()
+draw vlevel dc _view = do level <- varGet vlevel 
+                          {-putStrLn "Ich beginne zu zeichnen."-}
+                          {- mapM (drawPosition dc level) $ listofPts level -}
+                          draw' level (listofPts level) dc _view
+                          {-putStrLn "Ich habe fertig gezeichnet."-}
+                          {- debug level -}
+      
+      
+     
+debug :: Level -> IO ()
+debug level = dotodolist [ \_ -> print (statusNow level x) | x <- listofPts level ] 
+
+draw' :: Level -> [Pt] -> DC a -> b -> IO ()
+draw' level (x:xs) dc _view = do drawPosition level x dc
+                                 draw' level xs dc _view
+draw' _ [] _ _ = return ()
+
+
+
+drawPosition :: Level -> Pt -> DC a -> IO ()
+drawPosition level (x,y) dc = do case initl level (x,y) of 
+                                   Need i -> drawPick dc (x,y) $ Need $ i - length ([(xx,yy) | xx <- [x-1,x,x+1], yy <- [y-1,y,y+1]] `intersect` pathl level)
+                                   p -> drawPick dc (x,y) p                               
+                                 when (((x,y) `hasCondition` (Head ==)) level) $ drawPick dc (x,y) "Head"
+                                 when (((x,y) `hasCondition` (Tail ==)) level) $ drawPick dc (x,y) Tail
+                                 {- putStrLn $ "Koordinate "++show (x,y)++" gezeichnet." -}
+                              
+
+drawPick :: Drawable p => DC a -> Pt -> p -> IO () 
+drawPick  dc (x,y) p = do bm <- elemToPick p
+                          drawBitmap dc bm (Point ((x-1)*picsize) ((y-1)*picsize)) True []                                   
+  where                                  
+   elemToPick ::Drawable a => a -> IO (Bitmap ())
+   elemToPick x = return $ bitmap ("../assets/" ++ drawable x ++ ".png")
+           
---
+++ somekindofpuzzle/src/Logic.hs
@@ -0,0 +1,283 @@
+module Logic where
+import Prelude
+import System.Random
+import Data.List
+import Data.Array.IO
+import Control.Monad
+import Data.Maybe
+
+
+type Pt = (Int, Int)
+
+class Drawable a where
+    drawable :: a -> String
+
+data Level = Level { sizel :: (Int, Int),
+                     timel :: Int,
+                     initl :: Pt -> Element,
+                     statusl :: Int -> Pt -> [State],
+                     pathl :: [Pt] }
+       
+data Element = Start | Need Int | PortalBlue | PortalOrange | End | Empty | Oob | Wall deriving (Eq, Show)
+
+instance Drawable Element where
+  drawable Oob = error "Oob cannot be drawn"
+  drawable (Need i) = "Need"++show i
+  drawable el = show el
+
+data State =  Head | Tail | Blocked | InTimeMachine deriving (Eq, Show)
+
+instance Drawable State where
+  drawable = show
+
+{- Richtig hässlicher Workaround, um Strings (und nichts anderes) Drawable zu machen. -}
+class IsChar a where
+  toChar :: a -> Char
+instance IsChar Char where
+  toChar = id
+instance IsChar a => Drawable [a] where
+  drawable = fmap toChar
+  
+finito :: Level -> Bool
+finito level = initl level (head (pathl level)) == End && all (hasNeeds level) (listofPts level)
+
+hasNeeds :: Level -> Pt -> Bool
+hasNeeds level (x,y) = case initl level (x,y) of
+                      Need i -> i == length ([(xx,yy) | xx <- [x-1,x,x+1], yy <- [y-1,y,y+1]] `intersect` pathl level)
+                      _ -> True
+
+
+
+doinput level newhead   = let newtime = timel level + 1 in
+                          Level { sizel = sizel level,
+                                  timel = newtime,
+                                  initl = initl level,
+                                  statusl = 
+                                    \t -> if t == newtime
+                                             then updateState level newhead
+                                             else statusl level t,
+                                  pathl = newhead:pathl level
+                                }
+
+
+
+
+  
+input :: Level -> Pt -> Level
+input level newhead | friendScout24 level (head (pathl level)) newhead
+                        = doinput level newhead
+                    | (newhead `hasCondition` isSnake) level 
+                        = let newt = findSnake level newhead in
+                          Level { sizel = sizel level,
+                                  timel = newt,
+                                  initl = initl level,
+                                  statusl = statusl level,
+                                  pathl = drop (timel level - newt) $ pathl level
+                                }
+                    | otherwise = level
+  where findSnake :: Level -> Pt -> Int
+        findSnake level pt = case elemIndex pt (pathl level) of
+                                  Nothing -> error "snake is broken"
+                                  Just x -> timel level - x
+        
+
+
+
+statusNow :: Level -> (Pt -> [State])
+statusNow level = statusl level $ timel level
+
+infixl 4 `hasCondition`                                  
+hasCondition :: Pt -> (State -> Bool) -> Level -> Bool
+(pt `hasCondition` cond) level = any cond $ statusNow level pt     
+
+infixl 4 `hasntCondition`                                  
+hasntCondition :: Pt -> (State -> Bool) -> Level -> Bool
+(pt `hasntCondition` cond) level = all (not.cond) $ statusNow level pt                                
+                                  
+                                  
+isSnake :: State -> Bool
+isSnake Head = True
+isSnake Tail = True
+isSnake _ = False
+
+isBlocking :: State -> Bool
+isBlocking Tail = True
+isBlocking Head = True
+isBlocking Blocked = True
+isBlocking _ = False
+
+updateState :: Level -> Pt -> Pt -> [State]                                    
+updateState level newhead pt    = [ Tail | (pt `hasCondition` isSnake) level ] ++
+                                  [ Head | newhead == pt ] ++
+                                  [ Blocked | initl level pt == Oob || initl level pt == Wall ]
+{- 
+headrepeats :: Eq a => [a] -> Int
+headrepeats (x:x':xs) = if x==x' then headrepeats (x':xs)+1 else 1
+headrepeats xs = length xs
+  -}
+  
+listofPts :: Level ->[Pt]
+listofPts Level {sizel = (xy,yx) } = [(x,y) | x <- [1 .. xy],y <- [1 .. yx]]                                   
+                                     
+findElement :: Level -> Element -> Maybe Pt
+findElement level element = find ((== element).initl level) (listofPts level)
+                                                    
+friendScout24 :: Level -> Pt -> Pt -> Bool
+friendScout24 level (x,y) pt =  (pt `hasntCondition` isBlocking) level && (pt `elem` [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
+                                                                             || initl level pt == PortalBlue && initl level (x,y) == PortalOrange     
+                                                                             || initl level pt == PortalOrange && initl level (x,y) == PortalBlue)
+                            {- || etc -}
+
+dotodolist :: (Monad m, Foldable t) => t (g -> m g) -> m g
+dotodolist = foldl (>>=) (return undefined) {- WTF! Kann mir endlich mal einer erklären, wann undefined geht und wann nicht? gz. JonJon -}
+
+
+{- Ab hier: Level Creation. Eventuell sogar auslagern? -}
+
+data Prelevel = Prelevel { sizep :: (Int, Int),
+                           initp :: Pt -> Element,
+                           guaranteedpathp :: [Pt],
+                           freep :: [Pt],
+                           startp :: Pt
+                         }
+        
+createLevel :: Int -> IO Level
+createLevel s = do zf <- randomRIO (0,1)
+                   let _ = zf :: Int
+                   let tierlist = [1,2,3,4]
+                       todolist = [lbui tier s | tier <- tierlist] {- Dynamisch erstellte Todo-Liste -}
+                   finalprelevel <- dotodolist todolist
+                   let endresult = doinput Level { sizel = sizep finalprelevel,
+                                                 timel = 0,
+                                                 initl = initp finalprelevel,
+                                                 statusl = \t pt -> [],
+                                                 pathl = []
+                                        } $ startp finalprelevel    
+                   return endresult
+                                        
+  where lbui :: Int -> Int -> Prelevel -> IO Prelevel    
+        lbui 1 s _ = do xy <- randomRIO (2,10)
+                        yx <- randomRIO (2,6)
+                        let sizep = (xy,yx)
+                        startp <- randomTile sizep
+                        let freep = delete startp [(x,y) | x <- [1 .. xy],y <- [1 .. yx]] {- Meh. -}
+                        return Prelevel { sizep = sizep,
+                                          initp = \(x,y) -> case () of _ | startp == (x,y) -> Start
+                                                                          | 0 < x && x <= xy && 0 < y && y <= yx -> Empty
+                                                                          | otherwise -> Oob,
+                                          guaranteedpathp = [startp],
+                                          freep = freep,
+                                          startp = startp
+                                        }
+        
+        lbui 2 s pre = do {-end <- randomFreeTile pre-}
+                          prepre <- createWay pre
+                          return prepre {freep = delete (head $ guaranteedpathp prepre) $ freep prepre,
+                                         initp = \pt -> case () of
+                                                                    _ | pt == head (guaranteedpathp prepre) -> End
+                                                                      | otherwise -> initp prepre pt         
+                                        }
+        lbui 3 s pre = do shuffled <- shuffle $ freep pre \\ guaranteedpathp pre
+                          let (xy,yx) = sizep pre
+                              changing = take (quot (xy*yx - length (guaranteedpathp pre)) 2) shuffled
+                          return pre { initp = \pt -> if pt `elem` changing then Wall else initp pre pt,
+                                       freep = freep pre \\ changing
+                                     }
+        lbui 4 s pre = do shuffled <- shuffle $ freep pre
+                          let (xy,yx) = sizep pre
+                              changing = take (quot (length shuffled) 3) shuffled
+                          {-putStrLn $ show $ freep pre-}
+                          return pre { initp = \pt -> if pt `elem` changing then Need $ getNeeds (guaranteedpathp pre) pt else initp pre pt,
+                                       freep = freep pre \\ changing
+                                     }                                     
+                                                                           
+        getNeeds :: [Pt] -> Pt -> Int
+        getNeeds path (x,y) = length $ intersect [(xx,yy) | xx <- [x-1,x,x+1], yy <- [y-1,y,y+1]] path
+  
+  
+        randomFreeTile :: Prelevel -> IO Pt
+        randomFreeTile pre = fromMaybe (error "Keine freien Felder mehr.") (randomElement $ freep pre)        
+        
+        randomTile :: Pt -> IO Pt
+        randomTile (xy,yx) = do x <- randomRIO (1,xy)
+                                y <- randomRIO (1,yx)
+                                return (x,y)
+                                          
+  
+        createWay :: Prelevel -> IO Prelevel
+        createWay pre
+                    = do let path = guaranteedpathp pre 
+                             h = head path
+                         if initp pre h /= Empty
+                           then do newhead <- selectNeighbor pre
+                                   case newhead of
+                                     Nothing -> error "ALARM! ALARM!"
+                                     Just x -> createWay pre {guaranteedpathp = x:path}
+                           else do z <- randomRIO (1,30)
+                                   let int :: Int
+                                       int = z {- Bäh! -}
+                                   case () of
+                                    _ |z == 11 -> return pre
+                                      |z == 12 -> do  cando <- portalpossible pre
+                                                      case cando of
+                                                        Nothing -> do newhead <- selectNeighbor pre
+                                                                      {- trust in Gesetz der großen Zahlen? -}
+                                                                      case newhead of
+                                                                        Nothing -> return pre
+                                                                        Just x -> createWay pre {guaranteedpathp = x:path}
+                                                        Just p -> do {-zp <- randomRIO (1,2)-}
+                                                                     let intp :: Int
+                                                                         intp = 1 {-TODO-} 
+                                                                     createWay pre {guaranteedpathp = p:path,
+                                                                                    initp = \pt -> case () of
+                                                                                                      _ | pt == h -> if intp == 1 then PortalBlue else PortalOrange
+                                                                                                        | pt == p -> if intp == 1 then PortalOrange else PortalBlue
+                                                                                                        | otherwise -> initp pre pt,
+                                                                                    freep = freep pre \\ [p,h]}
+                                                                     
+                                      | otherwise -> do newhead <- selectNeighbor pre
+                                                        case newhead of
+                                                          Nothing -> return pre
+                                                          Just x -> createWay pre {guaranteedpathp = x:path}                                    
+
+          where portalpossible :: Prelevel -> IO (Maybe Pt)
+                portalpossible pre = do shuffled <- shuffle $ filter (hasNeighbor pre) $ freep pre \\ guaranteedpathp pre
+                                        {- putStrLn $ show shuffled -}
+                                        return $ listToMaybe shuffled 
+                
+                selectNeighbor :: Prelevel -> IO (Maybe Pt)
+                selectNeighbor pre
+                    = do shuffled <- shuffle $ preNeighbors pre $ head $ guaranteedpathp pre
+                         return $ listToMaybe shuffled
+                
+                preNeighbors :: Prelevel -> Pt -> [Pt]
+                preNeighbors pre (x,y)
+                  = let (xy,yx) = sizep pre in 
+                    filter (\(xx,yy) -> xx > 0 && xx <= xy && yy > 0 && yy <= yx) $ intersect [(x+1,y), (x-1,y), (x,y+1), (x,y-1)] (freep pre) \\ guaranteedpathp pre
+                
+                hasNeighbor :: Prelevel -> Pt -> Bool
+                hasNeighbor pre = not.null. preNeighbors pre
+ 
+        proposeElements :: Int -> [Element]
+        proposeElements s = [ Wall | i <- [0 .. (s + 1)] ]
+  
+        randomElement :: [a] -> Maybe (IO a)
+        randomElement [] = Nothing
+        randomElement xs = return $ do n <- randomRIO (0,length xs - 1)
+                                       return $ xs !! n
+          
+        shuffle :: [a] -> IO [a]
+        shuffle xs = do
+                ar <- newArray n xs
+                forM [1..n] $ \i -> do
+                    j <- randomRIO (i,n)
+                    vi <- readArray ar i
+                    vj <- readArray ar j
+                    writeArray ar j vi
+                    return vj
+          where
+            n = length xs
+            newArray :: Int -> [a] -> IO (IOArray Int a)
+            newArray n =  newListArray (1,n)
+
+            
